66551b08d82d5967a7ad1e9dd737e382e17bb425
Suhaib <suhaibwadi@hotmail.com>
2024-05-14 16:58:13 +0300

    update

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import {
   Button,
   ScrollView,
 } from "react-native";
-import { Formik } from "formik";
+import { useFormik } from "formik";
 import * as Yup from "yup";
 
 const JordanValidNumber = /^(\\+962|0)(7[789]|79|78|77|76|75)[0-9]{7}$/;
@@ -31,83 +31,90 @@ const validation = Yup.object().shape({
 
 const App = () => {
   const [showInfoOnScreen, setShowInfoOnScreen] = useState(false);
+
+  const formik = useFormik({
+    initialValues: { email: "", password: "", phoneNumber: "" },
+    validationSchema: validation,
+    onSubmit: (values) => {
+      setShowInfoOnScreen(true);
+      console.log("Form values ==>>", values);
+    },
+  });
+
+  const {errors,handleChange,values,handleSubmit,touched} = formik;
+
   return (
     <>
       <StatusBar barStyle="dark-content" />
       <SafeAreaView style={styles.container}>
         <ScrollView style={styles.FormContainer}>
           <Text style={styles.headTitle}>Demo Sign Up Form</Text>
-          <Formik
-            initialValues={{ email: "", password: "", phoneNumber: "" }}
-            validationSchema={validation}
-            onSubmit={(values) => {
-              setShowInfoOnScreen(true);
-              console.log("Form values ==>>", values);
-            }}
-          >
-            {({ handleChange, handleSubmit, values, errors, touched }) => (
-              <>
-                <Text style={styles.TextLabel}>Email Adress</Text>
-                <TextInput
-                  style={styles.TextInput}
-                  placeholder="Email Address"
-                  onChangeText={handleChange("email")}
-                  value={values.email}
-                  maxLength={25}
-                />
-                {touched.email && errors.email && (
-                  <Text style={styles.error}>{errors.email}</Text>
-                )}
-                <Text style={styles.TextLabel}>Phone Number</Text>
-                <TextInput
-                  style={styles.TextInput}
-                  placeholder="Jordanian Phone Number"
-                  keyboardType="phone-pad"
-                  onChangeText={handleChange("phoneNumber")}
-                  value={values.phoneNumber}
-                  maxLength={10}
-                />
-                {touched.phoneNumber && errors.phoneNumber && (
-                  <Text style={styles.error}>{errors.phoneNumber}</Text>
-                )}
-                <Text style={styles.TextLabel}>Password</Text>
-                <TextInput
-                  style={styles.TextInput}
-                  placeholder="Password"
-                  secureTextEntry
-                  onChangeText={handleChange("password")}
-                  value={values.password}
-                  maxLength={20}
-                />
-                {touched.password && errors.password && (
-                  <Text style={styles.error}>{errors.password}</Text>
-                )}
-                <Button title="Sign Up" onPress={handleSubmit} />
 
-                {/* this step is extra just to show on Screen the Email & Phone Number & Passowrd */}
-                {showInfoOnScreen && [
-                  <Text>
-                    Email =={" "}
-                    <Text style={styles.informationOutOnScreen}>
-                      {values.email}
-                    </Text>
-                  </Text>,
-                  <Text>
-                    Phone Number =={" "}
-                    <Text style={styles.informationOutOnScreen}>
-                      {values.phoneNumber}
-                    </Text>
-                  </Text>,
-                  <Text>
-                    Password =={" "}
-                    <Text style={styles.informationOutOnScreen}>
-                      {values.password}
-                    </Text>
-                  </Text>,
-                ]}
-              </>
-            )}
-          </Formik>
+          <Text style={styles.TextLabel}>Email Address</Text>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Email Address"
+            onChangeText={handleChange("email")}
+          
+            value={values.email}
+            maxLength={25}
+          />
+          {touched.email &&errors.email && (
+            <Text style={styles.error}>{errors.email}</Text>
+          )}
+
+          <Text style={styles.TextLabel}>Phone Number</Text>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Jordanian Phone Number"
+            keyboardType="phone-pad"
+            onChangeText={handleChange("phoneNumber")}
+          
+            value={values.phoneNumber}
+            maxLength={10}
+          />
+          {touched.phoneNumber && errors.phoneNumber && (
+            <Text style={styles.error}>{errors.phoneNumber}</Text>
+          )}
+
+          <Text style={styles.TextLabel}>Password</Text>
+          <TextInput
+            style={styles.TextInput}
+            placeholder="Password"
+            secureTextEntry
+            onChangeText={handleChange("password")}
+          
+            value={values.password}
+            maxLength={20}
+          />
+          {touched.password && errors.password && (
+            <Text style={styles.error}>{errors.password}</Text>
+          )}
+
+          <Button title="Sign Up" onPress={handleSubmit} />
+
+          {showInfoOnScreen && (
+            <View>
+              <Text>
+                Email =={" "}
+                <Text style={styles.informationOutOnScreen}>
+                  {values.email}
+                </Text>
+              </Text>
+              <Text>
+                Phone Number =={" "}
+                <Text style={styles.informationOutOnScreen}>
+                  {values.phoneNumber}
+                </Text>
+              </Text>
+              <Text>
+                Password =={" "}
+                <Text style={styles.informationOutOnScreen}>
+                  {values.password}
+                </Text>
+              </Text>
+            </View>
+          )}
         </ScrollView>
       </SafeAreaView>
     </>
@@ -118,7 +125,6 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: "center",
-    
   },
   FormContainer: {
     flex: 1,
@@ -133,7 +139,6 @@ const styles = StyleSheet.create({
   },
   TextInput: {
     maxWidth: 300,
-    maxLength:300,
     margin: 10,
     borderRadius: 5,
     borderColor: "black",
